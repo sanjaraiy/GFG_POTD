9116118339d089a6dc5d2373538de671d2471fd7
sanjaraiy <deepakrai78788@gmail.com>
2025-01-03 23:47:23 +0530

    added Count Subarrays with given XOR

---
+++ CountSubarrayswithgivenXOR.cpp
@@ -0,0 +1,14 @@
+// Count Subarrays with given XOR
+
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+        int xor_=0,ans=0;
+        unordered_map<int,int> mm;
+        for(int x:arr){
+            xor_^=x;
+            if(xor_==k)ans++;
+            ans+=mm[xor_^k];
+            mm[xor_]++;
+        }
+        return ans;
+    }
