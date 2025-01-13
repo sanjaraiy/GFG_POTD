e792e5dea1bad6e0aacb5fe25d1bc3a023a3e30f
sanjaraiy <deepakrai78788@gmail.com>
2025-01-13 23:46:05 +0530

    added Container With Most Water

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,13 @@
+// Container With Most Water
+
+int maxWater(vector<int> &arr) {
+         int s=0,e=arr.size()-1,maxarea=0,area=0;
+        while(s<e){
+            area=min(arr[s],arr[e])*(e-s);
+            maxarea=max(maxarea,area);
+            if(arr[s]<arr[e]) s++;
+            else e--;
+        }
+        return maxarea;
+    }
+
